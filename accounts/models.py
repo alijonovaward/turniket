024f64b26c2147faee1750fcfcb683609d8b8ef5
AzamjonAlijonov <alijonovrobo@gmail.com>
2026-03-25 17:51:22 +0500

    account appi qoshidli

---
+++ accounts/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    first_name = models.CharField(max_length=100, null=True, blank=True)
+    last_name = models.CharField(max_length=100, null=True, blank=True)
+    image = models.ImageField(upload_to='profiles/', null=True, blank=True)
+
+    balance = models.IntegerField(null=True, blank=True, default=0)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.username} {self.first_name} {self.last_name}"
